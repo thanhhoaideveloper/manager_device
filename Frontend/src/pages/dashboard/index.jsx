13f27eb872c64f791a15d92be47ff4a360b814de
Trần Thanh Sơn <ttson35997@gmail.com>
2023-06-19 11:34:32 +0700

    Get list user failed

--- Frontend/src/pages/dashboard/index.jsx
+++ Frontend/src/pages/dashboard/index.jsx
@@ -1,13 +1,22 @@
-import {Box, useTheme} from "@mui/material";
+import {Box} from "@mui/material";
 import Header from "../../components/Header";
-import {tokens} from "../../theme";
 
 import Grid from '@mui/material/Unstable_Grid2';
 import GridCard from '../../components/Layout/GridCard/Index';
 import {useSelector} from "react-redux";
+import userApi from '../../apis/userApi';
+import React, {useEffect, useState} from "react";
 
 const Dashboard = () => {
-    const users = useSelector(state => state.userReducer.users);
+    var [totalUser, setTotalUser] = useState(0);
+    useEffect(() => {
+        const users = async () => {
+            const data = await userApi.getListUser();
+            return data;
+        }
+        setTotalUser(users.length);
+    }, []);
+
     const devices = useSelector(state => state.deviceReducer.devices);
     const departments = useSelector(state => state.departmentReducer.departments);
 
@@ -16,7 +25,7 @@ const Dashboard = () => {
             <Header title="DASHBOARD" subtitle="Welcome to your dashboard"/>
             <Box sx={{margin: "10px 0"}}>
                 <Grid container spacing={2}>
-                    <GridCard xs={3} title={'Tổng số người dùng'} content={users.length}/>
+                    <GridCard xs={3} title={'Tổng số người dùng'} content={totalUser}/>
                     <GridCard xs={3} title={'Tổng số thiết bị'} content={devices.length}/>
                     <GridCard xs={3} title={'Tổng số phòng ban'} content={departments.length}/>
                     <GridCard xs={3} title={'Tổng số yêu cầu thêm thiết bị'} content={'200'}/>
